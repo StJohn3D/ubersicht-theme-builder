fe2a0ef8d5f916bcbe7820b4a0c3174072f26b44
St. John Peaster <stjohn3d@outlook.com>
2016-02-04 17:43:35 -0500

    started a padding css file - not sure if this is a good idea or not

--- ubersicht-theme-builder.widget/com/scripts/appendStyles.js
+++ ubersicht-theme-builder.widget/com/scripts/appendStyles.js
@@ -4,5 +4,6 @@ render: function (output) {
 	// For awesome CSS effects, http://enjoycss.com/start#textShadow
 	$('head').append('<link rel="stylesheet" type="text/css" href="' + utb.root + 'com/styles/glow.css">');
 	$('head').append('<link rel="stylesheet" type="text/css" href="' + utb.root + 'com/styles/position.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="' + utb.root + 'com/styles/padding.css">');
 	$('head').append('<link rel="stylesheet" type="text/css" href="' + utb.root + 'com/styles/text_colors.css">');
 }
--- ubersicht-theme-builder.widget/themes/habitica_theme/habitica_avatar.widget/habitica_avatar.js
+++ ubersicht-theme-builder.widget/themes/habitica_theme/habitica_avatar.widget/habitica_avatar.js
@@ -4,14 +4,11 @@ refreshFrequency: '5h', // in ms or '2 days', '1d', '10h', '2.5 hrs', '2h', '1m'
 
 
 render: function (output) { // Initial html setup
-	//"https://habitica.com/export/avatar-" + this.userID + ".png"
-	var dummyQuery = Date.now();
 	var opts = utb.themes.habitica;
 	this.userID = opts.userID;
 
 	return "<div class='container'>" +
-		"<image src='https://habitica.com/export/avatar-" + this.userID + ".png' class='center_V' />" +
-		//"<image src='https://habitica-assets.s3.amazonaws.com/avatars%2Fb0a89939-23da-4f3a-81d0-7aa3ce7e55b5.png?dummy=" + dummyQuery + "' class='center_V' />" +
+		"<image src='https://habitica.com/export/avatar-" + this.userID + ".png' class='corner_TL padd_10' />" +
 	"</div>";
 },
 
